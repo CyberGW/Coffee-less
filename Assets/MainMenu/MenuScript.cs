5e775f41e7f2fb4667fc959d20f1b9261652e58f
MariusB <marius.berger84@gmail.com>
2017-12-05 16:39:00 +0000

    MainMenu attempt 5000

---
+++ Assets/MainMenu/MenuScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button exitText;
+	public Button startText;
+
+	// Use this for initialization
+	void Start () {
+		
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+	}
+	
+	public void ExitPress(){
+		
+		quitMenu.enabled = true;
+		exitText.enabled = false;
+		startText.enabled = false;
+
+	}
+
+	public void NoPress(){
+		quitMenu.enabled = false;
+		exitText.enabled = true;
+		startText.enabled = true;
+	}
+
+	public void StartLevel(string newGameLevel){
+		SceneManager.LoadScene (newGameLevel);
+	}
+	public void ExitGame(){
+		
+		Application.Quit ();
+
+	}
+
+		
+}
